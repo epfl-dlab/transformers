Fix `next_indices` computation bug: replace true division by floor rounding.

src/transformers/generation_logits_process.py
@@ -669,7 +669,7 @@ class ValueLogitsProcessor(LogitsProcessor):
 
         next_token_scores = scores.view(-1, self.num_beams * self.num_tokens_per_beam) \
             .gather(dim=1, index=next_tokens_indices)
-        next_indices = (next_tokens_indices / self.num_tokens_per_beam).long()
+        next_indices = torch.div(next_tokens_indices, self.num_tokens_per_beam, rounding_mode='floor').long()
         next_tokens = next_preliminary_tokens.view(-1, self.num_beams * self.num_tokens_per_beam) \
             .gather(dim=1, index=next_tokens_indices)
 

src/transformers/generation_utils.py
@@ -2472,7 +2472,7 @@ class GenerationMixin:
                 next_token_scores, 2 * num_beams, dim=1, largest=True, sorted=True
             )
 
-            next_indices = (next_tokens / vocab_size).long()
+            next_indices = torch.div(next_tokens, vocab_size, rounding_mode='floor').long()
             next_tokens = next_tokens % vocab_size
 
             # stateless